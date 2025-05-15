0f6b7102261c528a8d0abf07aa26fa167d3f1d46
Adela I. Garay <adelaisabelgarayalvarado@gmail.com>
2025-05-14 18:42:27 -0600

    Update factoresprimos.js

--- factoresprimos.js
+++ factoresprimos.js
@@ -1,7 +1,1 @@
- Descomposición en Factores Primos
-¿Qué es la Descomposición en Factores Primos?
-La descomposición en factores primos consiste en representar un número como el
-producto de números primos.
-Ejemplos:
-• 18 = 2 × 3 × 3
-• 45 = 3 × 3 × 5
+
